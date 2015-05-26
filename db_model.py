e64fc9ca2e2b531c1dae559aa860e58e0a96a332
Aku <aku.visuri@invian.fi>
2015-05-26 18:56:35 +0300

    start works

--- db_model.py
+++ db_model.py
@@ -3,6 +3,7 @@ import sqlite3 as sql
 import time
 
 DATABASE_FILE = "ds2015.db"
+CHUNK_SIZE = 200
 
 def init():
     cur, conn = getCursor()
@@ -11,10 +12,12 @@ def init():
        (
        id INTEGER PRIMARY KEY ASC,
        hash TEXT,
+       created_time REAL,
        started_time REAL,
        solved_time REAL,
        solved INTEGER DEFAULT 0,
-       solution TEXT
+       solution TEXT,
+       solvable INTEGER DEFAULT 1
        )
     ''')
 
@@ -51,7 +54,7 @@ def getClients():
 def getUnsolvedHashes():
     result = []
     cur, conn = getCursor()
-    cur.execute("SELECT * FROM hashes WHERE solved = 0")
+    cur.execute("SELECT * FROM hashes WHERE solved = 0 AND solvable = 1")
     for row in cur:
         result.append(row)
     return result
@@ -66,39 +69,89 @@ def getSolvedHashes():
 
 
 def getProgress(client_id):
+    print "getprogress"
     hash_id = 0
     cur, conn = getCursor()
-    sql = "SELECT hash_working FROM clients WHERE client_id = " + client_id
+    sql = "SELECT hash_working FROM clients WHERE client_id = '" + str(client_id)
     cur.execute(sql)
-    if not row:
-        return None, None
+    found = False
     for row in cur:
-        hash_id =  row[0]
-    sql = "SELECT character, row FROM progress WHERE hash_id = " + hash_id
+        hash_id = row[0]
+        found = True
+    if not found:
+        return None, None
+    sql = "SELECT character, row FROM progress WHERE hash_id = '" + str(hash_id)
     cur.execute(sql)
     if not row:
         return None, None
     for row in cur:
         return row[0], row[1]
 
-def nextCharacter(hash_id):
+def getNextHash():
     cur, conn = getCursor()
-    cur.execute("SELECT character FROM progress WHERE hash_id = " + hash_id)
+    cur.execute("SELECT id, hash FROM hashes ORDER BY created_time LIMIT 1")
+    found = False
+    for row in cur:
+        print row[0]
+        hash_id = row[0]
+        target = row[1]
+        found = True
+        ## taanne paasee
+    if not found:
+        return None, None, None
+    ##nextChunk(hash_id)
+    sql = "SELECT * FROM progress WHERE hash_id = " + str(hash_id)
+    cur.close()
+    cur, conn = getCursor()
+    cur.execute(sql)
+    for row in cur:
+        print "searching for " + str(target)
+        return target, row[1], row[2]
+
+def nextChunk(hash_id):
+    print "nextchunk"
+    cur, conn = getCursor()
+    cur.execute("SELECT row FROM progress WHERE hash_id = " + str(hash_id))
+    found = False
+    print "elikkas"
+    for row in cur:
+        curRow = row[0]
+        found = True
+        print "found"
+    if not found:
+        return 0
+    cur.execute("UPDATE progress SET row = " + str(curRow + CHUNK_SIZE))
+    conn.commit()
+
+
+def nextCharacter(hash_id):
+    print "Nextcharacter"
+    cur, conn = getCursor()
+    cur.execute("SELECT character FROM progress WHERE hash_id = '" + str(hash_id))
     for row in cur:
         curChar = row[0]
     ## if not end of alphabets
     if ord(curChar) < 123:
         new_char = char(ord(curChar) + 1)
-    cur.execute("UPDATE progress SET character = " + new_char + ", row = 0 WHERE hash_id = " + hash_id)
+    else:
+        couldNotSolve(hash_id)
+        return 0
+    cur.execute("UPDATE progress SET character = '" + str(new_char) + "', row = 0 WHERE hash_id = '" + str(hash_id) + "'")
     conn.commit()
 
+def couldNotSolve(hash_id):
+    cur, conn = getCursor()
+    cur.execute("UPDATE hashes SET solvable = 0 WHERE hash_id = '" + str(hash_id) + "'")
+    conn.commit()
 
 def post_hash(hash):
     cur, conn = getCursor()
-    sql = "INSERT INTO hashes (hash, started_time) VALUES ('" + str(hash) + "'," + str(time.time()) + ")"
+    sql = "INSERT INTO hashes (hash, created_time) VALUES ('" + str(hash) + "'," + str(time.time()) + ")"
     cur.execute(sql)
     conn.commit()
     lastid = cur.lastrowid
+    cur.execute("INSERT INTO progress VALUES (" + str(lastid) + ",'a', 1)")
+    conn.commit()
     cur.execute("SELECT * FROM hashes WHERE id = " + str(lastid))
     for row in cur:
         return row
@@ -125,9 +178,10 @@ def connectClient(client_id):
 
 def disconnect(client_id):
     cur, conn = getCursor()
-    cur.execute("DELETE FROM clients WHERE client_id = '" + str(client_id) + "'")
+    cur.execute("DELETE FROM clients WHERE id = '" + str(client_id) + "'")
     conn.commit()
     conn.close()
+    print "Disconnected " + str(client_id)
     return "Deleted"
 
 def ping(client_id):
@@ -142,6 +196,18 @@ def getPing(client_id):
     cur.execute("SELECT last_ping FROM clients WHERE client_id = '" + str(client_id) + "'")
     for row in cur:
         return str(row[0])
+
+# remove nonresponding clients
+def killThemDead():
+    cur, conn = getCursor()
+    now = time.time()
+    cur.execute("SELECT id, last_ping FROM clients")
+    for row in cur:
+        # if client not responed for 60 seconds
+        if (now - row[1]) > 90:
+            cur.execute("DELETE FROM clients WHERE id ='" + str(row[0]) + "'")
+            conn.commit()
+
 
 def getCursor():
     c = sql.connect(DATABASE_FILE)
--- server.py
+++ server.py
@@ -1,18 +1,18 @@
+from argparse import _ActionsContainer
 from flask import Flask, url_for, request
 import json
 import db_model
 
 DEBUG = True
-CHUNK_SIZE = 200
 
 app = Flask(__name__)
-
 
 def setup():
     db_model.init()
 
 @app.route('/hash/', methods=['GET', 'POST'])
 def hash():
+    db_model.killThemDead()
     if request.method == 'POST':
         for k in request.form:
             resp = db_model.post_hash(k)
@@ -24,10 +24,15 @@ def hash():
 def join():
     for k in request.form:
         resp = db_model.connectClient(request.form[k])
-    print str(resp)
     return resp
 
-@app.route('/ping/<float:client_id>', methods=['GET', 'POST'])
+@app.route('/disconnect', methods=['POST'])
+def disconnect():
+    for k in request.form:
+        resp = db_model.disconnect(request.form[k])
+    return resp
+
+@app.route('/ping/<client_id>', methods=['GET', 'POST'])
 def ping():
     if method == 'POST':
         for k in request.form:
@@ -38,8 +43,40 @@ def ping():
             resp = db_model.getPing(request.form[k])
             return resp
 
-@app.route('/next/<float:client_id>')
+@app.route('/start_crack', methods=['POST'])
+def start_crack():
+    result = []
+    for k in request.form:
+        client_id = str(request.form[k])
+    if not client_id:
+        return "No client id given."
+    print "starting " + client_id
+    if client_id == "aaaaa":
+        print "Mita vittuaaa?"
+        return 0
+    target, character, progress = db_model.getNextHash()
+    if not character:
+        return "No hashes to solve!"
+    f = open("wordlists/dictionary_huge_" + character + ".dic", 'r')
+    rowcount = 0
+    for row in f.readlines():
+        ## end of file
+        if not row:
+            db_model.nextCharacter(hash_id)
+            break
+        if ((rowcount > progress) & (rowcount < progress + db_model.CHUNK_SIZE)):
+            result.append(row)
+        rowcount += 1
+
+    resp = dict()
+    resp["target"] = target
+    resp["words"] = result;
+    return json.dumps(resp)
+
+
+@app.route('/next/<client_id>')
 def next(client_id):
+    db_model.killThemDead()
     if not client_id:
         return "No client id given."
     result = []
@@ -47,27 +84,28 @@ def next(client_id):
     if character == None:
         return "Could not find progress for client_id = " + client_id
 
-    f = open("/wordlists/dictionary_huge_" + character + ".dic", 'r')
+    f = open("wordlists/dictionary_huge_" + character + ".dic", 'r')
     rowcount = 0
     for row in f.readlines():
         ## end of file
         if not row:
             db_model.nextCharacter(hash_id)
             break
-        if ((rowcount > progress) & (rowcount < progress + CHUNK_SIZE)):
+        if ((rowcount > progress) & (rowcount < progress + db_model.CHUNK_SIZE)):
             result.append(row)
 
     return json.dumps(result)
 
 @app.route('/')
 def server_ui():
+    db_model.killThemDead()
     resp = '''<link rel=stylesheet type=text/css href=''' + url_for('static', filename='ui.css') + '''>'''
     resp += '''<script type=text/javascript src=''' + url_for('static', filename='jquery-1.11.3.min.js') + '''></script>'''
     resp += '''<script type=text/javascript src=''' + url_for('static', filename='ui.js') + '''></script>'''
 
     resp += '''
     <div class='add_new_hash'>
-        Add new hash to bruteforce: <input type='text' id='new_hash' size='32'></input><div class='submit_button'>Submit</div>
+        Add new hash to bruteforce: <input type='text' id='new_hash' size='36'></input><div class='submit_button'>Submit</div>
     </div>
     '''
 
